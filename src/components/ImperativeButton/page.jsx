bd29353cb930878d679b1b94160015d3c5d45544
ryanfadhilah <ryan.fadhilah.acc@gmail.com>
2023-09-09 15:38:14 +0700

    imperative

--- src/components/ImperativeButton/page.jsx
+++ src/components/ImperativeButton/page.jsx
@@ -1,27 +1,32 @@
-"use client"
-import React, { forwardRef, useImperativeHandle, useState } from 'react'
+/* eslint-disable react/display-name */
+"use client";
+import React, { forwardRef, useImperativeHandle, useState } from "react";
 
 const ImperativeButton = forwardRef((props, fref) => {
-    const [text, setText] = useState(0)
+  const [text, setText] = useState(0);
 
-    useImperativeHandle(fref, () => ({
-        increment() {
-            setText(text + 1)
-        },
-        decrement() {
-            setText(text - 1)
-        }
-    }))
+  useImperativeHandle(fref, () => ({
+    increment() {
+      setText(text + 1);
+    },
+    decrement() {
+      setText(text - 1);
+    },
+  }));
 
-    return (
-        <section className='flex flex-col p-5 gap-5 bg-teal-950 rounded-xl'>
-            <button className='p-5 bg-teal-600 rounded-xl hover:bg-yellow-500' onClick={() => setText(text - 1)}>
-                Child Button ( - 1 )
-            </button>
-            <div className='flex justify-center mx-5 p-5 rounded-xl bg-white text-black'>Child State : {text}</div>
-        </section>
+  return (
+    <section className="flex flex-col p-5 gap-5 bg-teal-950 rounded-xl">
+      <button
+        className="p-5 bg-teal-600 rounded-xl hover:bg-yellow-500"
+        onClick={() => setText(text - 1)}
+      >
+        Child Button ( - 1 )
+      </button>
+      <div className="flex justify-center mx-5 p-5 rounded-xl bg-white text-black">
+        Child State : {text}
+      </div>
+    </section>
+  );
+});
 
-    )
-})
-
-export default ImperativeButton
+export default ImperativeButton;
